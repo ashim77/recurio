40d2e0037e02ad6a6b6d1641392bd04b20c21e70
Asim Baroy <simt.ashim@gmail.com>
2025-12-29 16:07:30 +0600

    update tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	vite: {
+		plugins: [tailwind()],
+	},
 });
